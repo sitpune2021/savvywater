090a8fd4505f84e44cc0f4efabc1105397b2c60f
sitpune2021 <sitsolutionspune@gmail.com>
2025-02-13 05:09:07 -0800

    Update App.php

--- app/Config/App.php
+++ app/Config/App.php
@@ -16,7 +16,7 @@ class App extends BaseConfig
      *
      * E.g., http://example.com/
      */
-    public string $baseURL = 'https://savvywater.sitsolutions.co.in/';
+    public  $baseURL = 'https://savvywater.sitsolutions.co.in/';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
